f3486eccde0c180f120f37b8f19c964d0ebadf8a
Anuj <anujgoel109@gmail.com>
2020-10-14 00:27:29 +0530

    Add input check

--- sqrt.c
+++ sqrt.c
@@ -15,6 +15,13 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
+
+	if(input < 0){
+		printf("Invalid input. Inpit should be non-negative\n");
+		printf("End of program. Exiting.\n");
+		exit(-1);
+	}
+
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.\n");
 	return(0);
